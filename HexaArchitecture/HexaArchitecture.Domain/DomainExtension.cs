d95437d597091f42b76c7b5cf4d342ac917780a7
Godson Ihemere <excanny@yahoo.com>
2021-06-08 12:52:23 +0100

    Add project files.

---
+++ HexaArchitecture/HexaArchitecture.Domain/DomainExtension.cs
@@ -0,0 +1,13 @@
+﻿using HexaArchitecture.DomainApi.Port;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HexaArchitecture.Domain
+{
+    public static class DomainExtension
+    {
+        public static void AddDomain(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(typeof(IRequestDeal<>), typeof(DealDomain<>));
+        }
+    }
+}
---
+++ HexaArchitecture/HexaArchitecture.Persistence.Adapter/PersistenceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HexaArchitecture.Persistence.Adapter.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HexaArchitecture.Persistence.Adapter
+{
+    public static class PersistenceExtensions
+    {
+        public static void AddPersistence(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("HexaArchConnInMemoryDb"));
+        }
+    }
+}
